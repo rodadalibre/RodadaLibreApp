a34b048d91f87dcaabe7d0bc6390acdfe1fb2977
rodrigocarreonc <rodrigocarreon21@gmail.com>
2026-02-15 20:16:29 -0600

    Remove directory 'theme'

---
+++ app/src/main/java/com/rodrigocarreon/rodadalibre/ui/view/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.rodrigocarreon.rodadalibre.ui.view
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
