210e8c34bc5b6699b690d9e6ea42b0fed12327ea
rodrigocarreonc <rodrigocarreon21@gmail.com>
2026-03-30 19:07:54 -0600

    Refactor AuthResponse name

--- app/src/main/java/com/rodrigocarreon/rodadalibre/data/model/AuthResponse.kt
+++ app/src/main/java/com/rodrigocarreon/rodadalibre/data/model/AuthResponse.kt
@@ -2,7 +2,7 @@ package com.rodrigocarreon.rodadalibre.data.model
 
 import com.google.gson.annotations.SerializedName
 
-data class LoginResponse(
+data class AuthResponse(
     @SerializedName("access_token") val access_token: String,
     @SerializedName("token_type") val token_type: String,
     @SerializedName("expires_in") val expires_in: Int,
